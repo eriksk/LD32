c70163b186ef9ec3c275d6e385c1b4e0ff54a101
erikskogl <erikskoglund88@gmail.com>
2015-04-18 20:06:49 +0200

    added splines

--- Assets/Game/Scripts/Bosses/PuckEaterAI.cs
+++ Assets/Game/Scripts/Bosses/PuckEaterAI.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
 using System;
 
-public class PuckEater : MonoBehaviour
+public class PuckEaterAI : MonoBehaviour
 {
+	private GameObject player;
+
+	void Start()
+	{
+		player = GameObject.Find("Player");
+	}
 
 	void Update()
 	{
+		var currentAngle = transform.rotation.eulerAngles.z;
+		var angleToPlayer = Mathf.Atan2(
+			player.transform.position.y - transform.position.y, 
+			player.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
+		var targetAngle = Mathf.LerpAngle(currentAngle, angleToPlayer, 0.1f);
 
+        transform.rotation = Quaternion.AngleAxis(targetAngle, Vector3.forward);
 	}
 }
